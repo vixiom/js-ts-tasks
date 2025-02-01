7404b43b66d0c6160e5a525a48689122008045de
vixiom <katin.vitalij@student.ehu.lt>
2025-02-01 21:27:39 +0200

    feat: 1 attempt

--- src/converter.ts
+++ src/converter.ts
@@ -5,9 +5,35 @@
  * @param {'m'|'mi'|'gr'|'pound'|'C'|'K'} to
  * @returns {number}
  */
-//module.exports.converter = function (value: number, from: string, to: string): number {
-
-//}
+module.exports.converter = function (value: number, from: string, to: string): number {
+    const convertRate = {
+      CToK: 273.15,
+      KToC: -273.15,
+      mTomi: 0.0006213711,
+      miTom: 1 / 0.0006213711,
+      grToPound: 0.002204624,
+      poundTogr: 1/ 0.002204624
+    }
+      if ((to === 'C' && from === 'K') || (from === 'C' && to === 'K')) {
+        if (to === 'C') {
+          return Number((value + convertRate.KToC).toFixed(2));
+        }
+        return Number((value + convertRate.CToK).toFixed(2));
+      }
+      else if ((to === 'm' && from === 'mi') || (from === 'm' && to === 'mi')) {
+        if (to === 'm') {
+          return Number((value * convertRate.miTom).toFixed(2));
+        }
+        return Number((value * convertRate.mTomi).toFixed(2));
+      }
+      else if ((to === 'gr' && from === 'pound') || (from === 'gr' && to === 'pound')) {
+        if (to === 'gr') {
+          return Number((value * convertRate.poundTogr).toFixed(2));
+        }
+        return Number((value * convertRate.grToPound).toFixed(2));
+      }
+      return value;
+}
 
 
 
